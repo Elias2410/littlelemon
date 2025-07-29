90a23a8c9c16741d5bac281a8a9c3e205b065d08
Elias <eliasyahya100@gmail.com>
2025-07-29 21:13:07 +0400

    Booking & Menu DB Tables Created

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(
+        validators=[
+            MaxValueValidator(6),
+            MinValueValidator(0)
+        ]
+    )
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.Name
+    
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    Inventory = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    
+    def __str__(self):
+        return self.Title
